d0066b17a776de3969f43849d43e0682cd91fb24
Kartik Goel <goel.kartik39@gmail.com>
2020-10-07 15:44:11 +0530

    Pushed changes for deployment

---
+++ src/Recommend.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "./App.css"
+
+const Recommend = props => {
+  console.log(props);
+  const [result,setResult] = useState([]);
+  
+  useEffect(() => {
+    Axios.post("https://rezonance-radioactive11.herokuapp.com/recommend", {
+      id: props.location.state.id
+    }).then(res => {
+	//   res.data.map((elem) => elem.isPlaying = false);
+      setResult(res.data);
+      console.log(res.data, "result");
+    });
+  }, []);
+
+  //Final
+
+  let currentAudio = new Audio();
+  let isPlaying = false;
+  let audio = new Audio();
+  
+   const playSong = (preview, id,index) => {
+
+	audio.src = preview;
+
+	if(!isPlaying)
+	{
+		isPlaying = true;
+		audio.play();
+		currentAudio.src = audio.src;
+	}
+	else
+	{
+		if(currentAudio.src === audio.src)
+		{
+			audio.pause();
+			isPlaying = false;
+		}
+		else
+		{
+			audio.play()
+			currentAudio.src = audio.src;
+			isPlaying = true;
+		}
+	}
+	
+  };
+
+  //bhai server restart krde
+  return (
+    <div>
+      <h3 className ="heading-recommend">Song Recommendations for {props.location.state.song} </h3> 
+     {result && (
+        <div className="container">
+          <div className="row">
+            {result.map((songs,index) => (
+              <div
+                className="col-lg-4 col-md-6 col-sm-6 col-sm-12"
+                key={songs.spotify_id}
+              >
+                <div className="profile-card-2">
+                  <img
+                    src={songs.image_url}
+                    // src = "https://i.scdn.co/image/ab67616d0000b2731cbd0d5849b51c79c99e7b87"
+                    className="img img-responsive"
+                    onClick={() => playSong(songs.preview, songs.spotify_id,index)}
+                  />
+				  
+                  <div className="profile-name">{songs.song_name}</div>
+                  <div className="profile-username">{songs.artist_name}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Recommend;
---
+++ src/Search.js
@@ -0,0 +1,124 @@
+import React,{useState} from 'react';
+import './App.css';
+import axios from "axios"
+import {useHistory} from "react-router-dom"
+import { useEffect } from 'react';
+
+const Search = () => {
+  const [search,setSearch] = useState("");
+  const [result,setResult] = useState([]);
+  const [random,setRandom] = useState([]);
+
+  const publicUrl = "https://rezonance-radioactive11.herokuapp.com/"
+
+  const history = useHistory();
+
+  useEffect(() => {
+    axios.get(publicUrl+"random")
+    .then((res) => {
+        setRandom(res.data);
+    })
+  },[])
+
+  const getResults = (e) => {
+    
+    setSearch(e.target.value);
+    console.log(e.target.value);
+
+    if((e.target.value).length >=4 && ((e.target.value).length) !== 0) {
+
+        axios.post("https://rezonance-radioactive11.herokuapp.com/search",{
+          search_param : e.target.value
+        },{
+          headers: {
+            "Content-Type": "application/json",
+          },
+		}).then((res) => {
+				console.log(res.data.search_results);
+				setResult(res.data.search_results);
+				setRandom(null);
+			})             
+    	}
+  	}
+
+  const getId = (id,song) => { 
+      console.log(id);
+        history.push({
+            pathname: '/recommend',
+            state: {id,song}
+      })
+  }
+    
+  console.log(random,"random",result,"result");
+  
+  
+  return   (
+    <div>
+
+    	<div className="main">
+			<div className="button-container">
+				<input type="text" placeholder="Search Songs or Artists" onChange = {(e) => getResults(e)} value = {search} />
+				<div className="search"></div>
+			</div>
+      </div>
+
+	  <div className ="heading-container">
+							<h1 className = "heading-recommend"> Random Songs</h1>
+		</div>  
+			{random ? (
+					<div className="container">
+					<div className="row">
+
+						{random.map((songs) => (
+							<div className="col-lg-4 col-md-6 col-sm-6 col-xs-12"  key ={songs.spotify_id}>
+						
+								<div className="profile-card-2">
+									<img 
+										src={songs.image_url}
+										className="img img-responsive"/>
+										
+									<div className="profile-name">{songs.song_name}</div>
+									<div className="profile-username">{songs.artist_name}</div>
+		
+								</div>
+							</div>
+							))
+						}
+		
+						</div>
+					</div>
+  			) : (
+				result ?  (     
+					<div className="container">
+					  <div className="row">
+		  
+						  {result.map((songs) => (
+							  <div className="col-lg-4 col-md-6 col-sm-6 col-xs-12"  key ={songs.spotify_id}>
+						  
+								  <div className="profile-card-2">
+									  <img 
+										  src={songs.image_url}
+										  className="img img-responsive" onClick={() => getId(songs.id,songs.song) }/>
+										  
+									  <div className="profile-name">{songs.song}</div>
+									  <div className="profile-username">{songs.artist}</div>
+		  
+								  </div>
+							  </div>
+							  ))
+						  }
+		  
+						  </div>
+					  </div> 
+				  ) : (
+					  <h1 > loading </h1>
+				  )
+			  )}
+			  
+      
+        </div>
+  	)
+
+}
+
+export default Search;
