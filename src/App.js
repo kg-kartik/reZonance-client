ceefcd50651c692660704090f6da1b7c0482e7b1
Kartik Goel <goel.kartik39@gmail.com>
2020-10-23 12:40:50 +0530

    Improved folder structure and improved console logs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React,{useState} from 'react';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 //Components
-import Search from "./Search.js"
-import Recommend from "./Recommend.js"
-import Landing from "./Landing.js"
+import Search from './pages/Search';
+import Recommend from './pages/Recommend';
+import Landing from './pages/Landing';
 
 const App = () => {
 	return (
-		
 		<Router>
 			<Switch>
-				<Route exact path ="/" component = {Landing} />
-				<Route exact path ="/search" component = {Search} />
-				<Route exact path ="/recommend" component = {Recommend} />
+				<Route exact path="/" component={Landing} />
+				<Route exact path="/search" component={Search} />
+				<Route exact path="/recommend" component={Recommend} />
 			</Switch>
 		</Router>
-	)
-}
+	);
+};
 
 export default App;
